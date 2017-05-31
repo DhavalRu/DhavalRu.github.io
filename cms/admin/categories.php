42f066f3aa8aff2035c4a9afe78d8f0900648a21
DhavalRu <dhavalrughani@hotmail.com>
2017-05-30 22:56:44 -0700

    Edit, delete categories + refactored code

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -13,38 +13,25 @@
                             <small>Author</small>
                         </h1>
                         <div class="col-xs-6">
-                            <?php 
-                                if(isset($_POST['submit'])) {
-                                    $cat_title = $_POST['cat_title'];
-                                    if ($cat_title == "" || empty($cat_title)) {
-                                        echo "This field should not be empty";
-                                    }
-                                    else {
-                                        $query = "INSERT INTO categories(cat_title) ";
-                                        $query .= "VALUE('{$cat_title}') ";
-                                        $create_category_query = mysqli_query($connection, $query);
-                                        if (!$create_category_query) {
-                                            die('QUERY FAILED'. mysqli_error($connection));
-                                        }
-                                    }
-                                }
-                            ?>
+                            <?php insert_categories(); ?>
                             <form action="categories.php" method="post">
-                            <label for="cat-title">Add Category</label>
                                 <div class="form-group">
+                                    <label for="cat-title">Add Category</label>
                                     <input class="form-control" type="text" name="cat_title">
                                 </div>
                                 <div class="form-group">
                                     <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                                 </div>
                             </form>
+
+                            <?php 
+                                if (isset($_GET['edit'])) {
+                                    $cat_id = $_GET['edit'];
+                                    include "includes/update_categories.php";
+                                }
+                            ?>
                         </div>
                         <div class="col-xs-6">
-                            <?php 
-                                //$query = "SELECT * FROM categories LIMIT 3";
-                                $query = "SELECT * FROM categories";
-                                $select_categories = mysqli_query($connection, $query);
-                            ?>
                             <table class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
@@ -53,15 +40,8 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php
-                                        while($row = mysqli_fetch_assoc($select_categories)) {
-                                            $cat_id = $row['cat_id'];
-                                            $cat_title = $row['cat_title'];
-                                            echo "<tr>";
-                                            echo "<td>{$cat_id}</td>";
-                                            echo "<td>{$cat_title}</td>";
-                                            echo "</tr>";
-                                    }
+                                    <?php findAllCategories();
+                                        deleteCategories();
                                     ?>
                                 </tbody>
                             </table>
