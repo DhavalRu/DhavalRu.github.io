9c340016e6a0df3b75e1018ae16ba360b6ada3fb
DhavalRu <dhavalrughani@hotmail.com>
2018-01-01 20:48:30 -0500

    Set up model + main component

--- CryptoMarketInfo/src/app/app.component.ts
+++ CryptoMarketInfo/src/app/app.component.ts
@@ -8,10 +8,16 @@ import { CmcModel } from './models/cmc.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public cmc = new CmcModel('');
+  public cmc: CmcModel[] = [];
   constructor(public cmcService: CmcHttpService) {
     this.cmcService.getTopTen().subscribe((data: any) => {
-      this.cmc = new CmcModel(data[0].name);
+      for (let i = 0; i < data.length; i++) {
+        this.cmc.push(new CmcModel(data[i].name, data[i].symbol, data[i].rank,
+          data[i].price_usd, data[i].price_btc, data[i]['24h_volume_usd'],
+          data[i].market_cap_usd, data[i].available_supply, data[i].total_supply,
+          data[i].percent_change_1h, data[i].percent_change_24h, data[i].percent_change_7d));
+      }
+      // console.log(this.cmc);
     });
   }
 }
--- CryptoMarketInfo/src/app/models/cmc.model.ts
+++ CryptoMarketInfo/src/app/models/cmc.model.ts
@@ -1,3 +1,6 @@
 export class CmcModel {
-    constructor(public name: string) {}
+    constructor(public name: string, public symbol: string, public rank: number,
+        public price_usd: number, public price_btc: number, public volume_day: number,
+        public market_cap: number, public avail_supply: number, public total_supply: number,
+        public change_hr: number, public change_day: number, public change_week: number) {}
 }
